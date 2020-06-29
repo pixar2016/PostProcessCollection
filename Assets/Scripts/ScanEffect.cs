cd6f1ec8d3c5fc4890c6a7b75a2c4d1a90e7751c
pixar2016 <360970742@qq.com>
2020-06-29 20:06:16 +0800

    add scan effect

---
+++ Assets/Scripts/ScanEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pixar
+{
+	[ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+	public class ScanEffect : MonoBehaviour
+	{
+		Material _material;
+		Shader _shader;
+		Camera _camera;
+
+		void Awake()
+		{
+			_camera = GetComponent<Camera>();
+			_camera.depthTextureMode = DepthTextureMode.Depth;
+		}
+		
+	    private void OnRenderImage(RenderTexture source, RenderTexture destionation)
+	    {
+	    	if(_material == null)
+	    	{
+	    		_shader = Shader.Find("Pixar/ScanEffectShader");
+	    		_material = new Material(_shader);
+	    		_material.hideFlags = hideFlags.DontSave;
+	    	}
+	    	// _material.SetFloat();
+	    	Graphics.Blit(source, destionation, _material);
+	    }
+	}
+
+}
+
